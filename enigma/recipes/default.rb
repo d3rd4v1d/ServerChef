998aee38eedaeadd42609760f521283a63df909b
NetzRebel <david@netzrebel.com>
2014-12-10 23:06:47 +0100

    Installing PEAR Memcached

--- enigma/recipes/default.rb
+++ enigma/recipes/default.rb
@@ -8,3 +8,17 @@
 #
 
 include_recipe 'geoip'
+
+# update the main pear channel
+php_pear_channel 'pear.php.net' do
+  action :update
+end
+
+# update the main pecl channel
+php_pear_channel 'pecl.php.net' do
+  action :update
+end
+
+php_pear "memcached" do
+  action :install
+end
